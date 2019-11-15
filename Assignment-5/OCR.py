31c53909f1cbe7dec15e10c3aab13362bf2e4614
Bendik Nordeng <bendiknor@gmail.com>
2019-11-15 18:12:26 +0100

    Updated example images

--- Assignment-5/OCR.py
+++ Assignment-5/OCR.py
@@ -8,11 +8,11 @@ from sklearn.model_selection import train_test_split
 
 images, data, target = tools.load_data()
 
-scaler, data = tools.scale(data)
-pca, feature_reduced_data = tools.reduce(data,40)
-
-X_train, X_test, y_train, y_test = train_test_split(feature_reduced_data, target, test_size=0.2, random_state=10)
-clf = tools.ANN(X_train,y_train)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.2, random_state=10)
+scaler, scaled_X_train = tools.scale(X_train)
+pca, reduced_X_train = tools.reduce(scaled_X_train,40)
+clf = tools.ANN(reduced_X_train,y_train)
 
 img1 = plt.imread('data/detection-images/detection-1.jpg')
 preds = tools.sliding_window(clf, img1, scaler, pca)
@@ -21,4 +21,3 @@ tools.plot_prediction_windows(img1, preds, (10,10))
 img2 = plt.imread('data/detection-images/detection-2.jpg')
 preds = tools.sliding_window(clf, img2, scaler, pca)
 tools.plot_prediction_windows(img2, preds, (15,10))
-
--- Assignment-5/SVM.py
+++ Assignment-5/SVM.py
@@ -1,66 +1,79 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from sklearn import svm
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
-import matplotlib.pyplot as plt
 import tools
-
 images, data, target = tools.load_data()
 
-scaler, data = tools.scale(data)
-pca, feature_reduced_data = tools.reduce(data, 40)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.2, random_state=10)
 
-X_train, X_test, y_train, y_test = train_test_split(feature_reduced_data, target, test_size=0.2, random_state=10)
+scaler, scaled_X_train = tools.scale(X_train)
+pca, reduced_X_train = tools.reduce(scaled_X_train, 40)
 
+from sklearn import svm
+
+from sklearn.model_selection import GridSearchCV
 params = {'C': [pow(10,x) for x in range(-1,3)], 'gamma': [0.01,0.001, 0.0001, 0.00001]}
 
 cv = GridSearchCV(param_grid=params, estimator=svm.SVC(random_state=10), cv=10, n_jobs=-1)
-cv.fit(X_train, y_train)
+cv.fit(reduced_X_train, y_train)
 
-print("Train accuracy:", cv.score(X_train, y_train))
-print("Test accuracy:", cv.score(X_test, y_test))
+print("Train accuracy:", cv.score(reduced_X_train, y_train))
+
+scaled_X_test = scaler.transform(X_test)
+reduced_X_test = pca.transform(scaled_X_test)
+
+print("Test accuracy:", cv.score(reduced_X_test, y_test))
 
 print(cv.best_params_)
 
 clf = svm.SVC(gamma = 0.001, C=10, probability=True)
-clf.fit(X_train, y_train)
-print("Train accuracy", clf.score(X_train, y_train))
-print("Test accuracy:",clf.score(X_test, y_test))
+clf.fit(reduced_X_train, y_train)
+print("Train accuracy", clf.score(reduced_X_train, y_train))
+print("Test accuracy:",clf.score(reduced_X_test, y_test))
 
-import random
+bad_images = []
+for i in range(len(X_test)):
+    if clf.predict([reduced_X_test[i]])[0] != y_test[i]:
+        bad_images.append(i)
+print("Wrongly classified images:",bad_images)
+
+import matplotlib.pyplot as plt
+import numpy as np
+X_test = [np.reshape(X_test[i], [20,20]) for i in range(len(X_test))]
 fig, axs = plt.subplots(2,3, figsize = (8,6))
-img = random.randint(0,7112)
-pred = clf.predict([feature_reduced_data[img]])
+example_images = [0, 101, 300, 619, 1000, 1212]
+img = example_images[0]
+
+pred = clf.predict([reduced_X_test[img]])
 axs[0,0].set_title('Prediction: ' + pred[0].capitalize())
-axs[0,0].imshow(images[img])
+axs[0,0].imshow(X_test[img])
 
-img = random.randint(0,7112)
-pred = clf.predict([feature_reduced_data[img]])
+img = example_images[1]
+pred = clf.predict([reduced_X_test[img]])
 axs[0,1].set_title('Prediction: ' + pred[0].capitalize())
-axs[0,1].imshow(images[img])
+axs[0,1].imshow(X_test[img])
 
-img = random.randint(0,7112)
-pred = clf.predict([feature_reduced_data[img]])
+img = example_images[2]
+pred = clf.predict([reduced_X_test[img]])
 axs[0,2].set_title('Prediction: ' + pred[0].capitalize())
-axs[0,2].imshow(images[img])
+axs[0,2].imshow(X_test[img])
 
-img = random.randint(0,7112)
-pred = clf.predict([feature_reduced_data[img]])
+img = example_images[3]
+pred = clf.predict([reduced_X_test[img]])
 axs[1,0].set_title('Prediction: ' + pred[0].capitalize())
-axs[1,0].imshow(images[img])
+axs[1,0].imshow(X_test[img])
 
-img = random.randint(0,7112)
-pred = clf.predict([feature_reduced_data[img]])
+img = example_images[4]
+pred = clf.predict([reduced_X_test[img]])
 axs[1,1].set_title('Prediction: ' + pred[0].capitalize())
-axs[1,1].imshow(images[img])
+axs[1,1].imshow(X_test[img])
 
-img = random.randint(0,7112)
-pred = clf.predict([feature_reduced_data[img]])
+img = example_images[5]
+pred = clf.predict([reduced_X_test[img]])
 axs[1,2].set_title('Prediction: ' + pred[0].capitalize())
-axs[1,2].imshow(images[img])
+axs[1,2].imshow(X_test[img])
 
 plt.show()
 
-tools.plot_confusion_matrix(y_test, clf.predict(X_test), clf.classes_, "Confusion matrix")
+tools.plot_confusion_matrix(y_test, clf.predict(reduced_X_test), clf.classes_, "Confusion matrix")
